d22bd69156a4c65cc0b3be70a757f0b32affecb6
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-11-03 01:37:26 -0700

    Add project files.

---
+++ TwoPlayerTraining/Patches/GameRunner_Update.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using Nick;
+using System.Collections.Generic;
+
+namespace TwoPlayerTraining.Patches
+{
+    [HarmonyPatch(typeof(GameRunner), "Update")]
+    class GameRunner_Update
+    {
+        static void Postfix(GameRunner __instance)
+        {
+            if (Plugin.Disabled.Value) return;
+            if (!__instance.SpawnedGame || __instance.SpawnedModeType != GameModeSetup.Mode.Endless) return;
+
+            var gameInstance = __instance.SpawnedGame;
+
+            if (!gameInstance.GetAgentFromPlayerIndex(1, out var playerTwo)) return;
+
+            if (playerTwo.TryGetControls(out var p2controls))
+            {
+                if (p2controls.controller.type == GameController.ctrlType.CPU)
+                {
+                    if (!gameInstance.GetAgentFromPlayerIndex(0, out var playerOne)) return;
+
+                    if (playerOne.TryGetControls(out var p1controls))
+                    {
+                        var controllers = new List<GameController>();
+                        Controllers.GetControllersMovingStick(ref controllers);
+
+                        foreach (var controller in controllers)
+                        {
+                            if (controller != p1controls.controller)
+                            {
+                                p2controls.controller = controller;
+                                p2controls.PrepareControls();
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TwoPlayerTraining/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using BepInEx.Configuration;
+using MenuCore;
+using System.Collections.Generic;
+
+namespace TwoPlayerTraining
+{
+    [BepInPlugin("com.steven.nasb.2playertraining", "Two Player Training", "1.0.0")]
+    [BepInDependency("com.pink.nasb.menucore", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+
+        internal static ConfigEntry<bool> Disabled;
+
+        void Awake()
+        {
+            if (Instance)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+            Instance = this;
+
+            Disabled = Config.Bind("Settings", "Disabled", false, "Disable the plugin.");
+
+            var harmony = new Harmony(Info.Metadata.GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            #region menu
+            CustomOptionsMenuHandler.InitiateMenu(() =>
+            {
+                var menu = new CustomOptionsMenu("Two-Player Training");
+
+                menu.CreateSelector(
+                    "disabled",
+                    "Disable",
+                    new List<string> { "Yes", "No" },
+                    Disabled.Value ? 0 : 1,
+                    SelectorType.RightSide,
+                    (_, value) =>
+                    {
+                        Disabled.Value = value == 0;
+                        Config.Save();
+                    });
+
+                CustomOptionsMenuHandler.CreateMenuTab(menu);
+            });
+            #endregion
+        }
+
+        #region logging
+        internal static void LogDebug(string message) => Instance.Log(message, LogLevel.Debug);
+        internal static void LogInfo(string message) => Instance.Log(message, LogLevel.Info);
+        internal static void LogWarning(string message) => Instance.Log(message, LogLevel.Warning);
+        internal static void LogError(string message) => Instance.Log(message, LogLevel.Error);
+        internal static void LogError(Exception ex) => Instance.Log($"{ex.Message}\n{ex.StackTrace}", LogLevel.Error);
+        private void Log(string message, LogLevel logLevel) => Logger.Log(logLevel, message);
+        #endregion
+    }
+}
